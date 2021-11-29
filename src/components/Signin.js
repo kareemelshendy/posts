fe2e8059cb25c2b2c4906f4cda3b215cbbffb9d9
Karem Elshendy <karemalshendy@gmail.com>
2021-11-29 21:38:42 +0200

    chat

---
+++ src/components/Signin.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+// // import {Button} from 'material-ui'
+// // import firebase from 'firebase'
+// // import { auth } from '../firebase'
+// function Signin() {
+
+//     function signinWithGoogle(){
+//         // const provider = new firebase.auth.GoogleAuthProvider()
+//         // auth.signInWithPopup(provider)
+//     }
+//     return (
+//         <div>
+//             <button onClick={signinWithGoogle} className='btns'>SignIn With Google</button>
+//         </div>
+//     )
+// }
+
+// export default Signin
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// import {initializeApp} from 'firebase/app';
+
+
+// const firebaseApp = initializeApp({
+//   apiKey: "AIzaSyCX6uBkHiTsJtExQWpk9gWEmX6xYzXhS-8",
+//   authDomain: "firechat-7dea1.firebaseapp.com",
+//   projectId: "firechat-7dea1",
+//   storageBucket: "firechat-7dea1.appspot.com",
+//   messagingSenderId: "657264005950",
+//   appId: "1:657264005950:web:faa0df23e75dd7f9e3126f",
+//   measurementId: "G-DEYJGYZWBX",
+// })
+
+// const db = firebaseApp.firestore()
+
+// const auth = firebase.auth()
+
+// export { db, auth }
